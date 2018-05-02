85468b9bd717a8a06f1eb50b8c0e310cfaaa1086
itsol <hiepld@itsol.vn>
2018-05-02 17:41:04 +0700

    Fix bug!

---
+++ iOS 3/SmartOffice/SmartOffice/MVC/Views/ViewTypeDropDown.h
@@ -0,0 +1,13 @@
+//
+//  ViewTypeDropDown.h
+//  SmartOffice
+//
+//  Created by Hiep Le Dinh on 5/2/18.
+//  Copyright © 2018 ITSOL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewTypeDropDown : UIView
+
+@end
---
+++ iOS 3/SmartOffice/SmartOffice/MVC/Views/ViewTypeDropDown.m
@@ -0,0 +1,21 @@
+//
+//  ViewTypeDropDown.m
+//  SmartOffice
+//
+//  Created by Hiep Le Dinh on 5/2/18.
+//  Copyright © 2018 ITSOL. All rights reserved.
+//
+
+#import "ViewTypeDropDown.h"
+
+@implementation ViewTypeDropDown
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
